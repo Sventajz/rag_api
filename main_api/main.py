03db20415fa0a213df7027be5e71de5732c7724f
Sven Tajz <sventajz3@gmail.com>
2024-05-11 14:42:13 +0200

    added conversation history and deleting conversation history to the API

--- main_api/main.py
+++ main_api/main.py
@@ -34,15 +34,15 @@ messages = [
     SystemMessage(content='You are a helpfull and friendly travel assistant.')
 ]
 
-# query ='whats the phone number of Parish of the Blessed Trinity in amsterdam?'
+
 
 
 def augmented_prompt(query: str):
     results = vectorStore.similarity_search(query,k=3)
     source_knowledge = '\n'.join([x.page_content for x in results])
 
-    augmented_prompt = f"""Using your learned knowledge answer the query, If you dont have that knowledge, answer the query using the context below
-
+    augmented_prompt = f"""Using your learned knowledge answer the query, If you dont have that knowledge, answer the query using the context below.
+    For better understanding of the user here is the conversation history: {messages}
     Contexts: {source_knowledge}
 
     Query: {query}"""
@@ -51,18 +51,19 @@ def augmented_prompt(query: str):
 app = FastAPI()
 
 @app.post('/')
-
 async def create_query(query: Query):
-    # Extract the query text from the Query object
     query_text = query.query
-
-    # Convert the query text to raw JSON string
     raw_query_text = json.dumps({"query": query_text})
 
-    # Pass the raw query text to augmented_prompt function
     prompt = HumanMessage(
         content=augmented_prompt(raw_query_text)
     )
     messages.append(prompt)
     res = chat(messages)
     return {res.content}
+
+
+@app.delete('/')
+async def delete():
+    global messages
+    messages = []
